e03a3160e307ed0b69c48fa27dcafca18ee54974
parthmp <parthparmaronline@gmail.com>
2026-06-17 01:19:49 +0530

    working on login - day end

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,9 @@
 <template>
+	<div class="pr-2">
+		<ThemeChanger></ThemeChanger>
+	</div>
 	<RouterView />
 </template>
 <script setup lang="ts">
-
+	import ThemeChanger from './components/general/ThemeChanger.vue';
 </script>
--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -1,14 +1,28 @@
 <template>
-	<div class="p-2 pt-0">
-		<ThemeChanger></ThemeChanger>
+	<div>
 		<Card class="w-full max-w-[550px] m-auto mt-10">
-			login
+			<template #title>Login</template>
+			<template #content>
+				
+				<label for="email">Email</label>
+				<InputText placeholder="Enter email" class="w-full" id="email"></InputText>
+				<br>
+				<br>
+				<label for="password">Password</label>
+				<Password placeholder="Enter password" id="password" toggleMask class="w-full" :inputStyle="{width:'100%'}"></Password>
+				
+				<br>
+				<br>
+				<Button class="w-full text-lg!">Login</Button>
+			</template>
+			
 		</Card>
 	</div>
 </template>
 
 <script setup lang="ts">
-import ThemeChanger from '../components/general/ThemeChanger.vue';
+
+import { InputText, Password, Button } from 'primevue';
 import { Card } from 'primevue';
 
 
